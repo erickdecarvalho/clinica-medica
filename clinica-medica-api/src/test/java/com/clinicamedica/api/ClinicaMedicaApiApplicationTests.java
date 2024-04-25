b62dba65cf280c26b432b121bccdc904f45c1db4
erick7amorim <erickamorimcarv@gmail.com>
2024-04-25 13:52:17 -0300

    projeto criado

---
+++ clinica-medica-api/src/test/java/com/clinicamedica/api/ClinicaMedicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicamedica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
