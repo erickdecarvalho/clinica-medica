fe05f55675f202511e2acc27dbf32fe6fc0e94b8
erick7amorim <erickamorimcarv@gmail.com>
2024-04-26 11:25:15 -0300

    repositório de médicos criado

--- clinica-medica-api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package com.clinicamedica.api.controller;
 
 import com.clinicamedica.api.medico.DadosCadastroMedico;
+import com.clinicamedica.api.medico.Medico;
+import com.clinicamedica.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
