149cd0a4c70da11abc17e9d2e92480d24b266030
erick7amorim <erickamorimcarv@gmail.com>
2024-04-26 15:40:41 -0300

    listagem de médicos

--- clinica-medica-api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/controller/MedicoController.java
@@ -1,14 +1,14 @@
 package com.clinicamedica.api.controller;
 
 import com.clinicamedica.api.medico.DadosCadastroMedico;
+import com.clinicamedica.api.medico.DadosListagemMedico;
 import com.clinicamedica.api.medico.Medico;
 import com.clinicamedica.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -20,6 +20,11 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
