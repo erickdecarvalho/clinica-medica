584f6c467c94dbc41b10eecff5d1e9cd0246fdb6
erick7amorim <erickamorimcarv@gmail.com>
2024-04-27 14:56:37 -0300

    crud de pacientes criado

---
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package com.clinicamedica.api.paciente;
+
+import com.clinicamedica.api.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+}
