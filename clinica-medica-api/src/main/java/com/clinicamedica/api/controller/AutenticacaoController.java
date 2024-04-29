01210cf73a7cabb635f833c308dcd4e0d6bc98fa
erick7amorim <erickamorimcarv@gmail.com>
2024-04-29 16:46:30 -0300

    jwt - melhorando o código de geração de token por boas práticas

--- clinica-medica-api/src/main/java/com/clinicamedica/api/controller/AutenticacaoController.java
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/controller/AutenticacaoController.java
@@ -2,6 +2,7 @@ package com.clinicamedica.api.controller;
 
 import com.clinicamedica.api.domain.usuario.DadosAutenticacao;
 import com.clinicamedica.api.domain.usuario.Usuario;
+import com.clinicamedica.api.infra.security.DadosTokenJWT;
 import com.clinicamedica.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,8 @@ public class AutenticacaoController {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 
---
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package com.clinicamedica.api.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
