2b507efb1c83a5fff7a6b727ad74b5479c07518c
erick7amorim <erickamorimcarv@gmail.com>
2024-04-30 14:15:24 -0300

    entidade, migrations, records e controller de consultas criados

---
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,20 @@
+package com.clinicamedica.api.domain.consulta;
+
+import com.clinicamedica.api.domain.medico.Especialidade;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+        Long idMedico,
+
+        @NotNull
+        Long idPaciente,
+
+        @NotNull
+        @Future
+        LocalDateTime data,
+
+        Especialidade especialidade) {
+}
