5a83abf6d661d7a7a53de54e9559f9c3b8cc1975
erick7amorim <erickamorimcarv@gmail.com>
2024-04-30 18:12:06 -0300

    lógica da regra de negócio para agendamento de consultas implementada

---
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/infra/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package com.clinicamedica.api.infra.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
