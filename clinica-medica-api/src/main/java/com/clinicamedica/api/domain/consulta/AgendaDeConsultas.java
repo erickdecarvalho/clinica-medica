f87d7a524438d8196dedf566076bea405c90246a
erick7amorim <erickamorimcarv@gmail.com>
2024-05-02 17:41:51 -0300

    correções quando a consulta é com um médico aleatório

--- clinica-medica-api/src/main/java/com/clinicamedica/api/domain/consulta/AgendaDeConsultas.java
+++ clinica-medica-api/src/main/java/com/clinicamedica/api/domain/consulta/AgendaDeConsultas.java
@@ -41,6 +41,10 @@ public class AgendaDeConsultas {
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var medico = escolherMedico(dados);
 
+        if (medico == null) {
+            throw new ValidacaoException("Não existe médico disponível nessa data!");
+        }
+
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
